d5f24d16507687fe0ccf11c25b6ef2be8dc5e0a7
kena26 <izaamarnavarro20@gmail.com>
2023-12-13 07:22:31 -0500

    Co-authored-by: Miguelin02 <Miguelin02@users.noreply.github.com>

--- Cine/src/main/java/com/example/Cine/EjemploApplication.java
+++ Cine/src/main/java/com/example/Cine/EjemploApplication.java
@@ -1,20 +1,56 @@
 package com.example.Cine;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.Cine")
+@ComponentScan(basePackages = {"com.example.Cine", "com.example.Cine.modelos", "com.example.Cine.Services"})
 public class EjemploApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EjemploApplication.class, args);
 	}
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 
     @Configuration
     public class CorsConfig {
